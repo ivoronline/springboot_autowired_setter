0fe9a5ff455aa4c5a98683af981ea421a4fe3a98
ivoronline <ivoronline@gmail.com>
2024-07-12 12:53:28 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_autowired_setter/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_autowired_setter/controllers/MyController.java
@@ -9,13 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
+  //PROPERTIES
   MyService myService;
 
+  //=======================================================================
+  // SETTER
+  //=======================================================================
   @Autowired
   public void setMyService(MyService myService) {
     this.myService = myService;
   }
 
+  //=======================================================================
+  // HELLO
+  //=======================================================================
   @ResponseBody
   @RequestMapping("/Hello")
   public String hello() {
