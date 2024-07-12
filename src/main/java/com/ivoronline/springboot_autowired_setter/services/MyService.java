6e2f28c9ea65f6ad37aa04e616ab77e3cc565107
ivoronline <ivoronline@gmail.com>
2024-07-12 13:14:34 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_autowired_setter/services/MyService.java
+++ src/main/java/com/ivoronline/springboot_autowired_setter/services/MyService.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyService {
 
-  //PROPERTIES
-  public String name = "John";
-
   //=======================================================================
   // SAY HELLO
   //=======================================================================
